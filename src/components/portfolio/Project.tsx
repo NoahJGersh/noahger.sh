ac9b7b8c85069d32209a7a8b350d38a966c41dfd
NoahJGersh <me@noahger.sh>
2025-10-05 15:49:10 -0700

    feat(portfolio): add source code link support and refactor typings

--- src/components/portfolio/Project.tsx
+++ src/components/portfolio/Project.tsx
@@ -1,4 +1,4 @@
-import type { ProjectProps } from "~/types/projects";
+import type { Portfolio } from "~/types/portfolio";
 import Technology from "./Technology";
 
 export default function Project({
@@ -8,12 +8,17 @@ export default function Project({
   cta,
   link,
   tech,
+  source,
   children,
-}: ProjectProps) {
+}: Portfolio.ProjectProps) {
   return (
     <div
       id={id}
-      className={`m-1 flex max-h-[250px] w-2xl flex-row overflow-hidden rounded-lg bg-neutral-300 shadow-md/20 dark:bg-neutral-800 dark:shadow-neutral-300`}
+      className={`
+        relative m-1 flex max-h-[250px] w-2xl flex-row overflow-hidden
+        rounded-lg bg-neutral-300 shadow-md/20
+        dark:bg-neutral-800 dark:shadow-neutral-300
+      `}
     >
       <div className="flex flex-col p-4">
         <h3 className="text-xl font-bold">{name}</h3>
@@ -31,14 +36,32 @@ export default function Project({
           href={link}
           target="_blank"
           rel="noopener noreferrer"
-          className={`link-invalid:cursor-default link-invalid:bg-neutral-700/50 link-invalid:text-neutral-300/50 link-valid:hover:bg-cyan-950 link-valid:hover:dark:bg-cyan-500 link-valid:hover:dark:text-neutral-100 w-min self-center rounded-sm bg-cyan-800 p-2 text-center text-sm font-semibold whitespace-nowrap text-cyan-400 dark:bg-cyan-400 dark:text-cyan-950`}
+          className={`
+            w-min self-center rounded-sm bg-cyan-800 p-2 text-center text-sm
+            font-semibold whitespace-nowrap text-cyan-400
+            dark:bg-cyan-400 dark:text-cyan-950
+            link-invalid:cursor-default link-invalid:bg-neutral-700/50
+            link-invalid:text-neutral-300/50
+            link-valid:hover:bg-cyan-950 link-valid:hover:dark:bg-cyan-500
+            link-valid:hover:dark:text-neutral-100
+          `}
         >
           {cta}
         </a>
       </div>
       {thumb ? (
-        <div className="flex min-h-full min-w-fit flex-col justify-center p-0 max-md:hidden">
+        <div className={`
+          flex min-h-full min-w-fit flex-col justify-center p-0
+          max-md:hidden
+        `}>
           <img src={thumb} className="w-full" />
+        </div>
+      ) : (
+        <></>
+      )}
+      {source ? (
+        <div className="absolute top-2 right-2 h-6 w-6">
+          <Technology id={source.host} url={source.url} isSource />
         </div>
       ) : (
         <></>
---
+++ src/types/portfolio.d.ts
@@ -0,0 +1,26 @@
+export namespace Portfolio {
+  export type TechConfig = Record<string, Technology>;
+
+  export interface Technology {
+    name: string;
+    logo: string;
+    logo_light?: string; // Light mode logo for better contrast
+    url: string;
+  }
+
+  interface ProjectSource {
+    host: name; // Should be a Technology name
+    url: string;
+  }
+
+  export interface ProjectProps {
+    id: string;
+    name: string;
+    thumb?: string;
+    cta: string;
+    link?: string;
+    tech?: string[];
+    source?: ProjectSource;
+    children: ReactNode | undefined;
+  }
+}
