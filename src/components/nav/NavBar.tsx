048195240eded99fda8334d8e6d7e291bb3cae4f
NoahJGersh <me@noahger.sh>
2025-10-05 13:12:47 -0700

    feat(nav): add proper styling and route matching to NavBar

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,5 +1,5 @@
 import * as fs from "node:fs";
-import { Link } from "@tanstack/react-router";
+import { Link, useMatchRoute } from "@tanstack/react-router";
 import { createServerFn, useServerFn } from "@tanstack/react-start";
 import { Navigation } from "~/types/navigation";
 import { useQuery } from "@tanstack/react-query";
@@ -12,29 +12,38 @@ const getPageData = createServerFn({ method: "GET" }).handler(
 );
 
 export default function NavBar() {
+  // Get navigation links from server config
   const getPages = useServerFn(getPageData);
-
   const { data } = useQuery({
     queryKey: ["pages"],
     queryFn: () => getPages(),
   });
 
+  // Get current path
+  const matchRoute = useMatchRoute();
+
   return (
-    <div className="static w-full">
-      <Link to={"/"}>
-        <h1>NoahGer.sh</h1>
-      </Link>
-      <div>
-        {data?.map((page, i) =>
-          page.path ? (
-            <Link key={i} to={page.path}>
-              {page.name}
-            </Link>
-          ) : (
-            <a key={i}>{page.name}</a>
-          ),
-        )}
-      </div>
+    <div className={`
+      sticky top-0 flex w-screen flex-row items-end mask-b-from-white
+      mask-b-from-75% mask-alpha p-4 backdrop-blur-lg
+    `}>
+      <h1 className="pr-8 text-3xl font-bold">NoahGer.sh</h1>
+      {data?.map((page, i) =>
+        page.path ? (
+          <Link
+            key={i}
+            to={page.path}
+            className={`
+              mr-4 text-2xl
+              ${matchRoute({ to: page.path }) ? `font-semibold text-cyan-500` : ""}
+            `}
+          >
+            {page.name}
+          </Link>
+        ) : (
+          <a key={i}>{page.name}</a>
+        ),
+      )}
     </div>
   );
 }
