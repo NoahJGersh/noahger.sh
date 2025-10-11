da6bfc9c0119a9d2e30d666029e201bb3cd1c9a8
NoahJGersh <me@noahger.sh>
2025-10-10 22:16:42 -0700

    feat(portfolio): add SportsBubble and Metalworks projects to portfolio

--- src/components/portfolio/Technology.tsx
+++ src/components/portfolio/Technology.tsx
@@ -3,6 +3,7 @@ import { createServerFn, useServerFn } from "@tanstack/react-start";
 import type { Portfolio } from "~/types/portfolio";
 import { useQuery } from "@tanstack/react-query";
 import { useMediaQuery } from "usehooks-ts";
+import { useHover } from "@uidotdev/usehooks";
 
 const getTechData = createServerFn({ method: "GET" }).handler(
   async (): Promise<Portfolio.TechConfig> => {
@@ -15,14 +16,26 @@ const getTechData = createServerFn({ method: "GET" }).handler(
 
 export default function Technology({
   id,
+  parentId,
+  subtechs,
   url,
-  isSource,
+  isSmall,
+  noMargin,
 }: {
   id: string;
+  parentId?: string;
+  subtechs?: string[];
   url?: string;
-  isSource?: boolean;
+  isSmall?: boolean;
+  noMargin?: boolean;
 }) {
+  // Check for light mode to use higher contrast icons
   const isLightMode = useMediaQuery("(prefers-color-scheme: light)");
+
+  // Track hover state so that the subtech popup can properly toggle
+  const [techRef, isTechHovered] = useHover();
+  const [subtechRef, isSubtechHovered] = useHover();
+
   const getTech = useServerFn(getTechData);
 
   const { data } = useQuery({
@@ -32,33 +45,79 @@ export default function Technology({
 
   // Let's not render anything if parsing fails,
   // or the tech isn't actually configured.
-  if (!data || !data[id]) return;
+  if (!data) return;
 
-  const { name, logo, logo_light, url: techUrl } = data[id];
+  // Attempt to find relevant config data
+  let techData: Portfolio.Technology;
+  if (parentId) {
+    const parent = data[parentId];
+    if (!parent.subtechs || !(id in parent.subtechs))
+      throw new Error(`${id} is not a subtech of ${parent}`);
+    techData = parent.subtechs[id];
+  } else {
+    if (!(id in data)) throw new Error(`${id} is not a valid technology`);
+    techData = data[id];
+  }
 
-  // Set separate (smaller) size for source code links
-  const size = isSource ? 4 : 8;
+  const {
+    name,
+    logo,
+    logo_light,
+    url: techUrl,
+    subtechs: dataSubtechs,
+  } = techData;
 
-  // Disable margins for source code links (absolute position)
-  const margins = isSource ? "" : "m-1 mt-2 mb-2";
+  // Use separate icon sizes for main, and source/subtech
+  const size = isSmall ? "h-6 w-6" : "h-8 w-8";
+
+  // Disable margins for small icons (they're positioned differently)
+  const margins = noMargin ? "" : "m-1 mt-2 mb-2";
 
   return (
-    <a
-      href={url ?? techUrl}
-      target="_blank"
-      rel="noopener noreferrer"
-      className={`
-        ${margins}
-        h-${size}
-        w-${size}
-      `}
-      title={name}
-    >
-      <img
-        src={isLightMode && !!logo_light ? logo_light : logo}
-        alt={name}
-        className="h-full w-full"
-      />
-    </a>
+    <div className={`
+      relative
+      ${margins}
+      ${size}
+    `} ref={techRef}>
+      <a
+        href={url ?? techUrl}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={name}
+        className="inline-block h-full w-full"
+      >
+        <img
+          src={isLightMode && !!logo_light ? logo_light : logo}
+          alt={name}
+          className="h-full w-full"
+        />
+      </a>
+      {subtechs && subtechs.length > 0 && dataSubtechs ? (
+        <div
+          hidden={!isTechHovered && !isSubtechHovered}
+          className="before:absolute before:top-8 before:h-2 before:w-8"
+        >
+          <div
+            className={`
+              before:absolute before:left-2 before:z-40 before:h-4 before:w-4
+              before:rotate-45 before:bg-neutral-700
+            `}
+            ref={subtechRef}
+          >
+            <div className={`
+              absolute top-[125%] left-[-125%] z-40 flex w-[350%] basis-auto
+              flex-row flex-wrap justify-center gap-x-1 gap-y-1 rounded-md
+              bg-neutral-700 p-1 shadow-xl
+            `}>
+              {subtechs.map((t, i) => (
+                <Technology key={i} id={t} parentId={id} noMargin />
+              ))}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
--- src/routes/portfolio.tsx
+++ src/routes/portfolio.tsx
@@ -23,14 +23,77 @@ function RouteComponent() {
           "nodejs",
           "typescript",
           "tailwindcss",
+          "go",
+          "postgres",
         ]}
         source={{
           host: "github",
           url: "https://github.com/NoahJGersh/noahger.sh",
         }}
       >
-        I built this website using TanStack. Renovations are ongoing as I
-        modernize and spruce things up.
+        I built this website using TanStack. The upcoming Vastest Sea section is
+        in further development, powered by a Go API and backed by a PostgreSQL
+        database. Renovations are ongoing as I modernize and spruce things up.
+      </Project>
+      <Project
+        id="watchsports"
+        name="WatchSports (SportsBubble)"
+        cta="View Online"
+        link="https://www.watchsports.app/"
+        tech={[
+          "aws",
+          "nextjs",
+          "react",
+          "nodejs",
+          "apollo",
+          "neo4j",
+          "typescript",
+          "serverless",
+          "terraform",
+        ]}
+        subtechs={{
+          aws: [
+            "acm",
+            "amplify",
+            "apigateway",
+            "cloudformation",
+            "cloudfront",
+            "cloudtrail",
+            "cloudwatch",
+            "cognito",
+            "dynamodb",
+            "eventbridge",
+            "iam",
+            "iamidentitycenter",
+            "lambda",
+            "managementconsole",
+            "route53",
+            "s3",
+            "sqs",
+          ],
+        }}
+      >
+        Full-stack development for SportsBubble&apos;s WatchSports app. Backend
+        work includes implementation, deployment, testing, and management of
+        serverless functions on AWS; numerous individual API, parser, and web
+        scraper integrations; and database management. Frontend work includes
+        development of a partner-facing B2B data management tool using Next.js.
+      </Project>
+      <Project
+        id="metalworks"
+        name="Cosmere RPG Implementation for FoundryVTT"
+        cta="Speak the Words"
+        link="https://www.foundryvtt.store/search?q=cosmere%20rpg"
+        tech={["foundryvtt", "typescript"]}
+        source={{
+          host: "github",
+          url: "https://github.com/the-metalworks/cosmere-rpg",
+        }}
+      >
+        I am an open source contributor to the design, implementation, and
+        testing of the official system for the Cosmere RPG on Foundry Virtual
+        Tabletop. I have also undergone data entry tasks for the premium content
+        modules, which adapt the rulebooks into a digital environment.
       </Project>
       <Project
         id="blusterin-badlands"
